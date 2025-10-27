7d13ae13015c0bceec1c6b968a99193ccea770d7
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-10-27 19:34:57 +0000

    V3

--- launchDB.sh
+++ launchDB.sh
@@ -4,5 +4,5 @@ aws cloudformation create-stack \
   --parameters ParameterKey=DBName,ParameterValue=tickets \
                ParameterKey=DBUser,ParameterValue=admin \
                ParameterKey=DBPassword,ParameterValue=Nicololo \
-               ParameterKey=SubnetIds,ParameterValue="subnet-0434313f93540400e,subnet-0ead6bed5978c2748,subnet-068de7ffb26d81cb6,subnet-0a935f102748c575f,subnet-01a43dd43ab0714c0,subnet-0758e7636ce16a70e" \
+               ParameterKey=SubnetIds,ParameterValue="" \
                ParameterKey=VpcId,ParameterValue=vpc-0cc4b8ec2faadc305 
